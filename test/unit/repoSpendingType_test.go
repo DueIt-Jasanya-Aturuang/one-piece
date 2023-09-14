ac09b58bd0faf37d1719b2ef94a8321afb35e669
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2023-09-14 12:16:16 +0700

    OPE-0805 | feature: unit test create spending type

---
+++ test/unit/repoSpendingType_test.go
@@ -0,0 +1,95 @@
+package unit
+
+import (
+	"context"
+	"database/sql"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/DueIt-Jasanya-Aturuang/one-piece/domain"
+	"github.com/DueIt-Jasanya-Aturuang/one-piece/internal/_repository"
+)
+
+func TestRepoSpendingTypeCreate(t *testing.T) {
+	db, mocksql, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	uow := _repository.NewUnitOfWorkRepositoryImpl(db)
+	spendingTypeRepo := _repository.NewSpendingTypeRepositoryImpl(uow)
+
+	query := regexp.QuoteMeta(`INSERT INTO m_spending_type (id, profile_id, title, maximum_limit, created_at, created_by, updated_at) 
+				VALUES ($1, $2, $3, $4, $5, $6, $7)`)
+	spendingType := &domain.SpendingType{
+		ID:           "test",
+		ProfileID:    "test",
+		Title:        "test",
+		MaximumLimit: 123,
+		CreatedAt:    0,
+		CreatedBy:    "test",
+		UpdatedAt:    0,
+	}
+	t.Run("SUCCESS", func(t *testing.T) {
+		mocksql.ExpectBegin()
+		mocksql.ExpectPrepare(query)
+		mocksql.ExpectExec(query).WithArgs(
+			"test", "test", "test", 123, 0, "test", 0,
+		).WillReturnResult(sqlmock.NewResult(1, 1))
+		mocksql.ExpectCommit()
+
+		err := spendingTypeRepo.OpenConn(context.TODO())
+		assert.NoError(t, err)
+		err = spendingTypeRepo.StartTx(context.TODO(), &sql.TxOptions{}, func() error {
+			err = spendingTypeRepo.Create(context.TODO(), spendingType)
+			assert.NoError(t, err)
+			return nil
+		})
+		assert.NoError(t, err)
+
+		err = mocksql.ExpectationsWereMet()
+		assert.NoError(t, err)
+	})
+}
+
+func TestRepoSpendingTypeUpdate(t *testing.T) {
+	db, mocksql, err := sqlmock.New()
+	assert.NoError(t, err)
+
+	mocksql.ExpectPrepare("")
+	defer db.Close()
+}
+
+func TestRepoSpendingTypeDelete(t *testing.T) {
+	db, mocksql, err := sqlmock.New()
+	assert.NoError(t, err)
+
+	mocksql.ExpectPrepare("")
+	defer db.Close()
+}
+
+func TestRepoSpendingTypeGetByID(t *testing.T) {
+	db, mocksql, err := sqlmock.New()
+	assert.NoError(t, err)
+
+	mocksql.ExpectPrepare("")
+	defer db.Close()
+}
+
+func TestRepoSpendingTypeGetByIDAndProfileID(t *testing.T) {
+	db, mocksql, err := sqlmock.New()
+	assert.NoError(t, err)
+
+	mocksql.ExpectPrepare("")
+	defer db.Close()
+}
+
+func TestRepoSpendingTypeGetAllByProfileID(t *testing.T) {
+	db, mocksql, err := sqlmock.New()
+	assert.NoError(t, err)
+
+	mocksql.ExpectPrepare("")
+	defer db.Close()
+}
